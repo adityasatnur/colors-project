768741c92d8415f12762abb1e68961425870e3a8
adityasatnur <adityasatnur@gmail.com>
2019-07-02 19:08:23 +0530

    Added basic Files

---
+++ src/Palette.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Palette extends Component {
+    state = {  }
+    render() { 
+        return ( <h1>Yo</h1> );
+    }
+}
+ 
+export default Palette;
