0f068eb2319f157bc06eecb81109860ea60c4d5b
adityasatnur <adityasatnur@gmail.com>
2019-07-02 19:19:33 +0530

    Test Commit

--- src/Palette.js
+++ src/Palette.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 class Palette extends Component {
     state = {  }
     render() { 
-        return ( <h1>Yo</h1> );
+        return ( <h1>aditya</h1> );
     }
 }
  
