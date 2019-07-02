6a5b7946aa0bc024b5a045bcb06e9b8c667bd889
adityasatnur <adityasatnur@gmail.com>
2019-07-02 19:12:17 +0530

    Updates

---
+++ precache-manifest.c87f60ce16574f2f31257ff9c975665b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a906b89e198874a51b53620837f6639",
+    "url": "/colors-project/index.html"
+  },
+  {
+    "revision": "f2894595b4143891cdb5",
+    "url": "/colors-project/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/colors-project/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f2894595b4143891cdb5",
+    "url": "/colors-project/static/js/main.d8aa9806.chunk.js"
+  },
+  {
+    "revision": "e12452dd58e09b1b41dd",
+    "url": "/colors-project/static/js/runtime~main.220ca8bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/colors-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
