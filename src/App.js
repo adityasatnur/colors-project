450f6dea02727ae336c00121a9051b2567c158e9
adityasatnur <adityasatnur@gmail.com>
2019-07-19 23:22:47 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router";
 import logo from "./logo.svg";
 import "./App.css";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./ColorHelpers";
 
-function App() {
-  console.log(generatePalette(seedColors[4]));
-  return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
-  );
+class App extends Component {
+  findPalette = (id) => {
+    return seedColors.find((palette) => {
+      return palette.id === id;
+    });
+  };
+  // console.log(generatePalette(seedColors[4]));
+  render() {
+    debugger;
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h2>yo</h2>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+      // <div className="App">
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
+    );
+  }
 }
-
 export default App;
